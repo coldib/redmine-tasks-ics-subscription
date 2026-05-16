e6396cdbe6508e176e0234874159245e9d85fd5a
Wolfgang Grim <wo@grim.at>
2026-05-16 16:55:16 +0000

    small issue

--- init.rb
+++ init.rb
@@ -1,11 +1,12 @@
 require 'redmine'
 
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
+
 Redmine::Plugin.register :redmine_tasks_ics_subscription do
   name        'Redmine Tasks ICS Subscription'
   author      'Wolfgang Lobo'
   description 'Exposes assigned Redmine issues as VTODO items in an ICS feed (CalDAV-compatible)'
   version     '1.0.0'
-
   settings :default => {
              'project_ids'                  => [],
              'project_ids_with_subprojects' => [],
